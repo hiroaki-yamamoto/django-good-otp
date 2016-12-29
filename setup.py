60e0d7b270e9be7f398a6c36308c31a92cf9b4a1
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-12-29 21:24:14 +0900

    Removed files

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup_kwargs.update({
     "long_description": long_desc,
     "packages": find_packages(exclude=["tests"]),
     "include_package_data": True,
-    "files": ["package.json"],
     "install_requires": dependencies,
     "zip_safe": False,
     "keywords": keywords,
