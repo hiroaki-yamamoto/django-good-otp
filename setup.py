79a0e7feccaf23e330dc63a09580238acf0c49a6
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-01-14 13:14:09 +0900

    Hello circleci

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@
 # coding=utf-8
 """Setup script."""
 
-from os.path import dirname, join
+from os.path import dirname, join, exists
 import sys
 from setuptools import setup, find_packages
 
 author = "Hiroaki Yamamoto"
 author_email = "hiroaki@hysoftware.net"
+version = "0.0.0"
+
+version_file = path.join(path.abspath(path.dirname(__file__)), "VERSION")
+
+if exists(version_file):
+    with open(version_file) as v:
+        version = v.read()
 
 if sys.version_info < (2, 7):
     raise RuntimeError("Not supported on earlier then python 2.7.")
@@ -25,7 +32,7 @@ setup(
         "One-Time-Password-Authentication for Django"
     ),
     license="MIT",
-    version="[VERSION]",
+    version=version,
     long_description=long_desc,
     url="https://github.com/hiroaki-yamamoto/django-good-otp",
     packages=find_packages(exclude=["tests"]),
@@ -35,7 +42,7 @@ setup(
     keywords=["python", "OTP", "Django"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
     ]
 )
