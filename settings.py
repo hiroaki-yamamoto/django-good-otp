c39c26dea395f6289017b793b3a05c66053e7632
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-04-08 16:45:44 +0900

    Adde 2.2 template

--- settings.py
+++ settings.py
@@ -16,6 +16,32 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     "django_otp.DjangoOTP",
 )
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect"
+            ]
+        }
+    }
+]
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATABASES = {
