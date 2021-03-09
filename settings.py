383fa6b21bc8376c249eeb8ea0fec4afc84f2cb2
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2021-03-09 11:29:18 +0900

    Include 2 context processors:
    'django.template.context_processors.debug',
    'django.template.context_processors.request',

--- settings.py
+++ settings.py
@@ -34,6 +34,8 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             "context_processors": [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "social_django.context_processors.backends",
